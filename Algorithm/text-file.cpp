b6847dc73f85d7bb2ac59be5a4d3cdccb9a673b9
Rehmanareeb <rehmanareeb24@gmail.com>
2023-12-04 22:22:20 +0500

    Amount of text Files and reading them

--- Algorithm/text-file.cpp
+++ Algorithm/text-file.cpp
@@ -2,6 +2,9 @@
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <fstream>
+
+using namespace std;
 
 int Checking_For_Text_Files()
 {
@@ -29,14 +32,49 @@ int Checking_For_Text_Files()
         closedir(d);
     }
 
-    // Print the result
+    // Print the number of text files found
     if (txtFileCount == 0)
     {
-        std::cout << "No text files found in the current directory." << std::endl;
+        cout << "No text files found in the current directory." << endl;
     }
     else
     {
-        std::cout << "Found " << txtFileCount << " text file(s) in the current directory." << std::endl;
+        cout << "Found " << txtFileCount << " text file(s) in the current directory." << endl
+             << endl;
+    }
+
+    // Open the directory again to read the files
+    d = opendir(".");
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            if (strstr(dir->d_name, ".txt") != NULL)
+            {
+                if (stat(dir->d_name, &fileStat) == 0)
+                {
+                    if (S_ISREG(fileStat.st_mode))
+                    {
+                        ifstream file(dir->d_name);
+                        if (file.is_open())
+                        {
+                            cout << "Contents of " << dir->d_name << ":\n";
+                            string line;
+                            while (getline(file, line))
+                            {
+                                cout << line << '\n';
+                            }
+                            file.close();
+                        }
+                        else
+                        {
+                            cout << "Unable to open file " << dir->d_name << endl;
+                        }
+                    }
+                }
+            }
+        }
+        closedir(d);
     }
 
     return 0;
